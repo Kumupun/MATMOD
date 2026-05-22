afe11093a353931c216ab3ed71e8f64d9c31b7a6
Kumupun <nastynski@gmail.com>
2026-05-22 13:49:15 +0300

    Дебаг та покращений точний розрахунок та вивід

--- func.py
+++ func.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-def solve_wave_equation(L, T, C) -> tuple:
+def solve_wave_equation(params) -> tuple:
     '''СІТКОВИЙ МЕТОД - Розв'язання хвильового рівняння
     РОЗБИВАЄМО ПРОСТІР НА СІТКУ, ВИКОРИСТОВУЄМО ДИСКРЕТНІ ПРИБЛИЖЕННЯ ДЛЯ ЧИСЛЕННОГО РОЗВ'ЯЗАННЯ'''
+    L, T, C = float(params[0]), float(params[1]), float(params[2])
 
-    nx = int(L*20)          # РОЗБИТТЯ НА X (20 точок на одиницю довжини)
-    nt = int(T*100)          # РОЗЬИТТЯ НА T (100 кроків на одиницю часу)
+    nx = int(L*20)          # РОЗБИТТЯ НА X (100 точок на одиницю довжини)
+    nt = int(T*100)          # РОЗБИТТЯ НА T (100 кроків на одиницю часу)
 
     dx = 2*L / (nx - 1)
     dt = T / nt
@@ -16,22 +17,36 @@ def solve_wave_equation(L, T, C) -> tuple:
     
     x = np.linspace(-L, L, nx)
     t = np.linspace(0, T, nt)
-    y = np.zeros((nt, nx))
 
-    u0 = lambda x: np.exp(-x**2)            # ПОЧАТКОВА УМОВИ умови (тут треба придумати як реалізувати ввід )
+    Y = np.zeros((nt, nx)) 
+
+    u0 = lambda x: np.sin(np.pi * x / L)     # ПОЧАТКОВі УМОВИ умови (тут треба придумати як реалізувати ввід ) (eval)
+    
     u1 = lambda x: np.zeros_like(x)
 
-    def exact_solution(x, t):               # ТОЧНЕ РІВНЯННЯ ДЛЯ ПЕРЕВІРКИ ТІЛЬКИ КОЛИ u1 = 0
-        return 0.5 * (u0(x - C*t) + u0(x + C*t))
+    def exact_solution(x, t):                            # ФОРМУЛА ДАЛАМБЕРА ДЛЯ БУДЬ ЯКОГО u0 u1
+        res = 0.5 * (u0(x - C * t) + u0(x + C * t))
+
+        # чисельний інтеграл
+        integral_part = np.zeros_like(x)
+
+        for i, xi in enumerate(x):
+            s = np.linspace(xi - C*t, xi + C*t, 100)  # розбиття
+            integral_part[i] = np.trapezoid(u1(s), s)
+
+        res += (1 / (2 * C)) * integral_part
+        return res
 
     Y = np.zeros((nt, nx))
 
     Y[0, :] = u0(x)                          # ПОЧАТКОВИЙ ШАР
+
+    Y[:, 0] = 0                         # КРАЙОВІ УМОВИ (Діріхле поки, а так теж ввід треба придумати) (тут можна через input)
+    Y[:, -1] = 0
+    
     for i in range(1, nx-1):
         Y[1, i] = (Y[0, i] + dt * u1(x[i]) + (lamd**2 / 2) * (Y[0, i+1] - 2*Y[0, i] + Y[0, i-1]))
 
-    y[:, 0] = 0                         # КРАЙОВІ УМОВИ (Діріхле поки, а так теж ввід треба придумати)
-    y[:, -1] = 0
 
     for n in range(1, nt-1):        # ОСНОВНИЙ ЦИКЛ РОЗВ'ЯЗАННЯ
         for i in range(1, nx-1):
@@ -40,7 +55,12 @@ def solve_wave_equation(L, T, C) -> tuple:
     t_final = t[-1]
 
     Y_exact = exact_solution(x, t_final)
-    Y_numeric = Y[-1]
+    Y_numeric = Y[-1, :]
+
+    print("max Y[0]:", np.max(Y[0]))
+    print("max Y[1]:", np.max(Y[1]))  # ДОДАНО ДЛЯ ДІАГНОСТИКИ  
+    print("lambda:", lamd)
+    print("max final:", np.max(Y[-1]))
 
     err = np.max(np.abs(Y_exact - Y_numeric))
     err2 = np.mean((Y_exact - Y_numeric)**2)
--- wibble_wobble.py
+++ wibble_wobble.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 import func
 import visual
 
@@ -7,14 +6,20 @@ def input_parameters() -> tuple:
     '''ВВІД ПАРАМЕТРІВ - Вручну або з файлу '''
 
     choice = input("Введіть тип вводу параметрів \n1 - Вручну \n2 - З файлу \nВаш вибір: ")
+    
     match choice:
         case '1':
-            L = float(input("Введіть довжину області (L): "))
-            T = float(input("Введіть час моделювання (T): "))
-            C = float(input("Введіть швидкість поширення (c): "))
+            try:
+                L = float(input("Введіть довжину області (L): "))
+                T = float(input("Введіть час моделювання (T): "))
+                C = float(input("Введіть швидкість поширення (c): "))
+            except ValueError:
+                print("Помилка вводу. Будь ласка, введіть числові значення.")
+                return None, None, None
+            
         case '2':
             filename = input("Введіть шлях до файлу з параметрами: ")
-            if filename == "":
+            if filename.strip() == "":
                 print("Шлях не вказано. Використовуються параметри за замовчуванням test.txt")
                 filename = "test.txt"
             try:
@@ -26,22 +31,34 @@ def input_parameters() -> tuple:
             except Exception as e:
                 print(f"Помилка при читанні файлу: {e}")
                 return None, None, None
-    
     return L, T, C 
 
+def final_output(params, Y_exact, Y_numeric, err, err2):
+    '''ВИВІД РЕЗУЛЬТАТІВ - Вивід точного та чисельного розв'язку, а також помилок'''
+
+    #ФОРМАТ ВИВОДУ МАТРИЦЬ  
+    np.set_printoptions(suppress=True, precision=2)                          # ВИВІД , 2 ДЕСЯТКОВИХ ЧИСЛА               
+    np.set_printoptions(formatter={'float_kind': lambda x: f"{x:.3e}"})      # ВИВІД У НАУКОВОМУ ФОРМАТІ (НАПРИКЛАД 1.23e-04) З 4 ДЕСЯТКОВИМИ ЧИСЛАМИ
+
+    print(f"Введені параметри: L={params[0]}, T={params[1]}, C={params[2]}")
+    print(f"Точне рішення в кінцевий момент часу: \n{Y_exact}\n")
+    print(f"Чисельне рішення в кінцевий момент часу: \n{Y_numeric}\n")
+    print(f"Максимальна похибка: {err}")
+    print(f"Середньоквадратична помилка: {err2}")
 
 def main():
-    L, T, C = input_parameters()
+    params = input_parameters()
+    if params == (None, None, None):
+        raise ValueError("Невірні параметри.")
     
-    result = func.solve_wave_equation(L, T, C)
+    (x, Y, dt, Y_exact, Y_numeric, err, err2) = func.solve_wave_equation(params)
+    
+    final_output(params, Y_exact, Y_numeric, err, err2)
 
-    visual.visual(result[0], result[1], result[2])
-    
-    print(f"Введені параметри: L={L}, T={T}, C={C}")
-    print(f"Точне рішення в кінцевий момент часу: \n{result[3]}\n")
-    print(f"Чисельне рішення в кінцевий момент часу: \n{result[4]}\n")
-    print(f"Максимальна похибка: {result[5]}")
-    print(f"Середньоквадратична помилка: {result[6]}")
+    visual.visual(x, Y, dt)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except BaseException as e:
+        print(f"Сталася помилка: {e}")
